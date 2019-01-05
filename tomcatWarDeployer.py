b986f1e2a3e190a3e7dbc2a8e2bf461e6909abef
Mariusz <mb@binary-offensive.com>
2019-01-06 00:24:16 +0100

    Welcome in 2019!

--- tomcatWarDeployer.py
+++ tomcatWarDeployer.py
@@ -19,7 +19,7 @@ from __future__ import print_function
 #   On Linux instances the file can be found at:
 #       /usr/share/metasploit-framework/lib/msf/core/payload/jsp.rb
 #
-# Mariusz B. / MGeeky, '16-18
+# Mariusz B. / MGeeky, '16-19
 #
 
 import re
